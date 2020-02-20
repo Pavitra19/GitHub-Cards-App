0352ef3f4de213d248809c0254f751ad4005a6d9
Pavitra19 <pavitra.kurseja@gmail.com>
2020-02-20 13:29:12 -0500

    Add user input

--- Readme.js
+++ Readme.js
@@ -30,10 +30,23 @@ class Card extends React.Component {
 }
 
 class Form extends React.Component {
+    state = {userName: ''};
+    handleSubmit = (event) => {
+
+        //so page won't refresh on submit
+        event.preventDefault();
+        console.log(this.state.userName);
+    };
+
     render() {
         return (
-            <form action="">
-                <input type="text" placeholder="GitHub username"/>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" 
+                value={this.state.userName}
+                onChange={event => this.setState({userName: event.target.value})}
+                placeholder="GitHub username"
+                required
+                />
                 <button>Add card</button>
             </form>
         );
